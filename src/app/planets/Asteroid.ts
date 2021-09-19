18fc03030e22877c684f8313788cdc8e386ecbb5
Jon Wheeler <wheelibin@gmail.com>
2021-09-19 07:15:43 +0100

    Added asteroid entity

---
+++ src/app/planets/Asteroid.ts
@@ -0,0 +1,14 @@
+import { Entity } from "./Entity";
+
+export class Asteroid extends Entity {
+  protected _dispose(): void {}
+  protected getMapColour(height: number, y: number): number[] {
+    const ratio = height / 255;
+
+    const { r, g, b } = this.params.colour!;
+
+    const rgba = [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255), 255];
+
+    return [rgba[0] * ratio, rgba[1] * ratio, rgba[2] * ratio, rgba[3]];
+  }
+}
